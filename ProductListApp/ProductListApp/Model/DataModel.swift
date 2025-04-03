455f18b9fbc86bb18bca90b8abdbdadeb998ed3f
Chandan Swain <134497712+chandan-swain@users.noreply.github.com>
2025-04-03 21:16:40 +0530

    Add files via upload

---
+++ ProductListApp/ProductListApp/Model/DataModel.swift
@@ -0,0 +1,22 @@
+//
+//  DataModel.swift
+//  ProductListApp
+//
+
+import Foundation
+import Foundation
+
+struct Rating: Codable {
+    let rate: Double
+    let count: Int
+}
+
+struct Product: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let category: String
+    let image: String
+    let rating: Rating
+}
